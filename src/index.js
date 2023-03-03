196d1e905351cdfabb7eedeea841e68927a4442e
uttamsaha <uttamsaha982@live.com>
2023-03-03 12:22:46 +0600

    upload code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TransactionProvider } from './Context/TransactionContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <TransactionProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+      <App/>
+      </BrowserRouter>
+    </React.StrictMode>
+  </TransactionProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
